0d8c6d1c16c6cb7d37ee706e5e188ced48b640f8
Andrew Cho <choxandrew@gmail.com>
2015-12-14 03:23:28 -0600

    got triplePair function working

--- PE485.hs
+++ PE485.hs
@@ -1,19 +1,16 @@
 import Control.Applicative
 
---sexyNumberChecker :: Int -> Bool
-sexyNumberChecker n = and (fmap (/= 0 ) a) --equal to 6 equal to 12, make fmap stop computing when there is a false
-   where a = (flip mod) <$> (fmap ((+1).(*2)) [1.. (truncate . (flip (/) 2). (subtract 1) . sqrt . (+9) $ fromIntegral n)]) <*> [n]
+triplePair :: Int -> Bool
+triplePair n = and [z | x <- (b n), let y = (n `mod` x), let z = if  y /= 0  &&  y /= (-6) `mod` x && y /=(-12) `mod` x then True else False]  --equal to 6 equal to 12, make fmap stop computing when there is a false
+--   where a = (flip mod) <$> (b n) <*> [n]
 
 b :: Int -> [Int]
-b n = (fmap ((+1).(*2)) [1.. (truncate . (flip (/) 2). (subtract 1) . sqrt . (+9) $ fromIntegral n)])
+b n = (fmap ((+1).(*2)) [1.. (truncate . (flip (/) 2). (subtract 1) . sqrt . (+12) $ fromIntegral n)])
 
+
+-- around 9x slower
 c :: Int -> [Int]
 c n = [x | x <- [3.. (truncate . sqrt . (+9) $ fromIntegral n)], odd x]
 
---
-main = do
-    let x = length $ b 99999999999999
-    let y = length $ c 99999999999999
-    putStrLn $ show y
 
 engineerChecker n   = undefined
---
+++ Readme.txt
@@ -0,0 +1,1 @@
+Me using Haskell to solve Project Euler problems
