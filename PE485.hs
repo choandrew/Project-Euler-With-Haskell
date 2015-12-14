05dbe296ab0cc9d7c2d2b66e6f3a905e0be22c33
Andrew Cho <choxandrew@gmail.com>
2015-12-14 00:02:44 -0600

    updated tests for b and c

--- PE485.hs
+++ PE485.hs
@@ -10,11 +10,10 @@ b n = (fmap ((+1).(*2)) [1.. (truncate . (flip (/) 2). (subtract 1) . sqrt . (+9
 c :: Int -> [Int]
 c n = [x | x <- [3.. (truncate . sqrt . (+9) $ fromIntegral n)], odd x]
 
-
-main = do 
-  x <- b 99999999
--- x <- c 99999999
-  putStrLn "hi"
-
+--
+main = do
+    let x = length $ b 99999999999999
+    let y = length $ c 99999999999999
+    putStrLn $ show y
 
 engineerChecker n   = undefined
